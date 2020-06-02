e81a28003efb66aae021f72b44e746ef6898bdba
borischen <bycxen@gmail.com>
2020-06-02 13:43:08 +0800

    change project name, fix clear input issue

--- src/views/ChatPage.js
+++ src/views/ChatPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, InputGroup, FormControl, Row, Col } from 'react-bootstrap'
+import { Button, InputGroup, FormControl, Row, Col } from 'react-bootstrap';
 import ChatContainer from './ChatContainer';
 import ChatInfo from './ChatInfo';
 import {socket} from "../utils/socket";
@@ -9,33 +9,24 @@ export default function ChatPage(props) {
     const [inputVal, setInputVal] = useState('');
     const [info, setInfo] = useState('請按下開始');
     const [connected, setConnected] = useState(false);
-    const [messages, setMessages] = useState([])
+    const [messages, setMessages] = useState([]);
+    const [key, setKey] = useState('');
 
     useEffect(() => {
-        socket.on('chat message', msg => {
-            console.log('on chat message', msg)
-            setMessages(oldMessages => [...oldMessages, {message: msg, inbound: true, date: moment()}])
-        });
-        socket.on('connectSuccess', function(){
+        socket.on('chat message', msg => setMessages(oldMessages => [...oldMessages, {message: msg, inbound: true, date: moment()}]));
+        socket.on('connectSuccess', () => {
             setConnected(true)
             setInfo('連線成功, 請開始你的表演');
             setTimeout(() => {
                 setInfo('');
             }, 3000);
-            console.log(`A user is connected`)
         });
         socket.on('chat history', msg => {
             setConnected(true)
             setInfo('');
-            msg.forEach((item) => {
-                setMessages(oldMessages => {
-                    return [...oldMessages, {message: item.msg, inbound: item.inbound, date: moment(item.date)}]
-                })
-            })
-            console.log(msg);
+            msg.forEach(item => setMessages(oldMessages => [...oldMessages, {message: item.msg, inbound: item.inbound, date: moment(item.date)}]));
         });
-        socket.on('chat end', function(){
-            console.log('chat end')
+        socket.on('chat end', () => {
             setInfo('對方離開了, 請按下開始');
             setConnected(false);
         });
@@ -47,7 +38,6 @@ export default function ChatPage(props) {
             setInfo('連線中');
             socket.emit('chat start');
         }
-        console.log('start chat')
     }
 
     const leaveChat = () => {
@@ -57,16 +47,16 @@ export default function ChatPage(props) {
             setInfo('已離開, 請按下開始');
             socket.emit('chat end');
         }
-        console.log('leave chat')
     }
 
     const sendMessage = () => {
         if (connected) {
             if (inputVal !== ''){
-                console.log('sending msg', inputVal);
                 socket.emit('chat message', inputVal);
-                setMessages(oldMessages => [...oldMessages, {message: inputVal, inbound: false, date: moment()}])
+                setMessages(oldMessages => [...oldMessages, {message: inputVal, inbound: false, date: moment()}]);
             }
+            setInputVal('');
+            setKey(Math.random()); // work around to clear input content after submit
         }
         return false;
     }
@@ -87,7 +77,7 @@ export default function ChatPage(props) {
             <div className="mt-2 mx-4" style={{height: '85%'}}>
                 <ChatContainer messages={messages} />
                 <ChatInfo showMsg={info}/>
-                <InputGroup className="mb-3" value={inputVal} onChange={(e)=>setInputVal(e.target.value)}>
+                <InputGroup className="mb-3" value={inputVal} onChange={(e)=>setInputVal(e.target.value.trim())} key={key}>
                     <FormControl/>
                     <InputGroup.Append>
                         <Button variant="outline-secondary" onClick={sendMessage}>送出</Button>
